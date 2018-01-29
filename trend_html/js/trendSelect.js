5abca7c234f36d861437fafccfdb34651e204cbb
王广哲 <guangzhe1@staff.sina.com.cn>
2018-01-29 15:33:23 +0800

    trends page init

---
+++ trend_html/js/trendSelect.js
@@ -0,0 +1,130 @@
+/**
+ * Created by wgz on 2018/1/28.
+ */
+function addstep(selectedstep)
+{
+    if(document.getElementByIdx_x("tr1td").innerHTML=="")
+        document.getElementByIdx_x("tr1td").innerHTML=selectedstep;
+    else if(document.getElementByIdx_x("tr2td").innerHTML=="")
+        document.getElementByIdx_x("tr2td").innerHTML=selectedstep;
+    else
+        document.getElementByIdx_x("tr3td").innerHTML=selectedstep;
+}
+
+function addRowToTable(){
+    appendOneRow(table.rows);
+}
+
+function deleteRow() {
+
+}
+
+function getKeywordsToRefreshTable() {
+    keywords = ["test", "test"];
+    // var callback = function (result) {
+    //     var keywords = result["keywords"];
+    //     if (isArray(keywords)) {
+            for (var i=0;i<keywords.length;i++) {
+                appendOneRow(i);
+            }
+    //     }
+    // }
+    // httpGetAsync("http://google/trends/getKeywords", callback)
+}
+
+function appendOneRow(i) {
+    var keywordInput = document.createElement("input");
+    keywordInput.type = "text";
+    keywordInput.name = "keyword_input";
+    var keywordCell = document.createElement("td");
+    keywordCell.append(keywordInput);
+
+    var regionOption1 = document.createElement("option");
+    regionOption1.value = "China";
+    regionOption1.text = "中国";
+    var regionOption2 = document.createElement("option");
+    regionOption2.value = "US";
+    regionOption2.text = "美国";
+    var regionSelect = document.createElement("select");
+    regionSelect.style = "width:90px";
+    regionSelect.name = "region_select";
+    regionSelect.appendChild(regionOption1);
+    regionSelect.appendChild(regionOption2);
+    var regionCell = document.createElement("td");
+    regionCell.appendChild(regionSelect);
+
+    var durationOption1 = document.createElement("option");
+    durationOption1.value = "China";
+    durationOption1.text = "中国";
+    var durationOption2 = document.createElement("option");
+    durationOption2.value = "US";
+    durationOption2.text = "美国";
+    var durationSelect = document.createElement("select");
+    durationSelect.style = "width:90px";
+    durationSelect.name = "duration_select";
+    durationSelect.appendChild(durationOption1);
+    durationSelect.appendChild(durationOption2);
+    var durationCell = document.createElement("td");
+    durationCell.appendChild(durationSelect);
+
+    var intervalOption1 = document.createElement("option");
+    intervalOption1.value = "China";
+    intervalOption1.text = "中国";
+    var intervalOption2 = document.createElement("option");
+    intervalOption2.value = "US";
+    intervalOption2.text = "美国";
+    var intervalSelect = document.createElement("select");
+    intervalSelect.style = "width:90px";
+    intervalSelect.name = "duration_select";
+    intervalSelect.appendChild(intervalOption1);
+    intervalSelect.appendChild(intervalOption2);
+    var intervalCell = document.createElement("td");
+    intervalCell.id = "row"+(i+1);
+    intervalCell.appendChild(intervalSelect);
+
+    var del_btn = document.createElement("input");
+    del_btn.name = "del_btn";
+    del_btn.type = "button";
+    del_btn.value = "删除";
+    del_btn.onclick = deleteRow;
+
+    var del_btn_div = document.createElement("div");
+    del_btn_div.className = "btn_box floatR";
+    del_btn_div.appendChild(del_btn);
+
+    var currentRow = document.createElement("tr");
+    currentRow.appendChild(keywordCell);
+    currentRow.appendChild(regionCell);
+    currentRow.appendChild(durationCell);
+    currentRow.appendChild(intervalCell);
+    currentRow.appendChild(del_btn_div);
+
+    var queryTabel = document.getElementById("key_table");
+    queryTabel.appendChild(currentRow);
+}
+
+function httpGetAsync(theUrl, callback) {
+    $.ajax({
+        url:theUrl,
+        dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+        success:callback,
+        error:function(){
+            alert("Error");
+        }
+    });
+}
+
+function httpPostAsync(theUrl, data, callback) {
+    $.ajax({
+        type: 'POST',
+        url: theUrl,
+        data: data,
+        success: callback,
+        dataType: jsonp
+    });
+
+}
+
+function isArray(object){
+    return Object.prototype.toString.call(object)=='[object Array]';
+}
